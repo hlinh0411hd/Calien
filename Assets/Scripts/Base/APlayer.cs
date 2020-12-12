dc90e889b584b02cf30725bf4f4f20a049c82cc7
hlinh0411hd <hlinh0411hd@gmail.com>
2020-12-12 16:47:22 +0700

    update

---
+++ Assets/Scripts/Base/APlayer.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class APlayer : MonoBehaviour
+{
+
+    public float speed;
+
+    protected float hor;
+    protected float vert;
+
+
+    public float timeDelayFire;
+    protected float currentTimeDelayFire;
+
+
+    protected void FixedUpdate()
+    {
+        hor = Input.GetAxis("Horizontal");
+        vert = Input.GetAxis("Vertical");
+        Move();
+
+
+        if (Input.GetButton("Fire1"))
+        {
+            if (currentTimeDelayFire < 0)
+            {
+                OnFire();
+                currentTimeDelayFire = timeDelayFire;
+            }
+        }
+
+        if (Input.GetButton("Fire2"))
+        {
+            DoAction();
+        }
+    }
+
+    protected abstract void Move();
+    protected abstract void OnFire();
+    protected abstract void DoAction();
+}
---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : APlayer
+{
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    protected override void OnFire()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    protected override void Move()
+    {
+        Vector2 position = transform.position;
+        position.x += hor * speed;
+        position.y += vert * speed;
+        transform.position = position;
+    }
+
+    protected override void DoAction()
+    {
+        Debug.Log("HIHI");
+    }
+}
